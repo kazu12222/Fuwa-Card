bb5720fab02f27164402af731d5d4ad123ecae25
Scarlet1107 <shougo.ushiro@gmail.com>
2024-10-18 11:49:47 +0900

    install shad cn

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
-
-export default function Home() {
+const Home = () => {
   return (
-    <div className="text-2xl text-center">
-         ふわっとカード
-          </div>
+    <main>
+      <h1 className="text-2xl mt-2 text-center">ふわっとカード</h1>
+      <button className="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">
+        ...
+      </button>
+    </main>
   );
-} 
+};
+
+export default Home;
